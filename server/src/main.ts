ae832feeabbe88118e7980ad8cf78011f3a2ce9c
zangqingan <18367319791@163.com>
2024-06-11 15:52:24 +0800

    feat:配置swagger接口文档

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 配置swagger
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('会议室预定系统')
+    .setDescription('练手项目')
+    .setVersion('1.0')
+    .addBearerAuth() // 接口增加token认证
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-docs', app, document, {
+    jsonDocumentUrl: 'api-docs.json', // json文档地址
+  }); // 设置访问接口地址--> http://localhost:PORT/api-docs#/ 查看swagger文档
   await app.listen(3000);
 }
 bootstrap();
